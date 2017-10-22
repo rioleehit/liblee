f16e51fa595f40a6ded64f2c9d846986992d80b2
rioleehit <rioleehit@foxmail.com>
2017-10-22 18:43:07 +0800

    修改 get set 【set 传入引用】【get 返回值 跟 返回引用】

--- include/get_set.hpp
+++ include/get_set.hpp
@@ -5,51 +5,74 @@
 namespace lee{
 	
 #define T_REF const T&
-template<class T>
-class Get {
-public:
-	Get(std::function<T_REF()> get) :m_get(get) {}
-	operator T_REF() { return m_get(); }
-	std::string toString() { return std::to_string(m_get()); }
-protected:
-	//Get(Get) {}
-	std::function<T_REF()> m_get;
-};
-template<>
-class Get <std::string>{
-public:
-	Get(std::function<const std::string&()> get) :m_get(get) {}
-	operator const std::string&() { return m_get(); }
-	std::string toString() { return m_get(); }
-protected:
-	//Get(Get) {}
-	std::function<const std::string&()> m_get;
-};
 
 template<class T>
 class Set {
 public:
-	Set(std::function<void(T)> set) :m_set(set) {}
-	void operator=(T v){ m_set(v); }
+	Set(std::function<void(const T&)>& set) :m_set(set) {}
+	Set(std::function<void(const T&)>&& set) :m_set(set) {}
+	void operator=(const T& v) { m_set(v); }
+	void operator=(const T&& v) { m_set(v); }
 protected:
-	//Set(Set) {}
-	std::function<void(T)> m_set;
+	Set(const Set&) = delete;
+	std::function<void(const T&)> m_set;
 };
-	
+
+template<class T>
+class Get {
+public:
+	Get(std::function<T()>& get) :m_get(get) {}
+	Get(std::function<T()>&& get) :m_get(get) {}
+	T val() { return m_get(); }
+	operator T() { return m_get(); }
+	std::string toString() { return std::to_string(m_get()); }
+protected:
+	Get(const Get&) {}
+	std::function<T()> m_get;
+};
+template<>
+class Get <std::string>{
+public:
+	Get(std::function<std::string()> get) :m_get(get) {}
+	std::string val() { return m_get(); }
+	operator std::string() { return m_get(); }
+	std::string toString() { return m_get(); }
+protected:
+	Get(const Get&) {}
+	std::function<const std::string()> m_get;
+};
+
+template<class T>
+class RefGet:public Get<T&>{
+public:
+	RefGet(std::function<T&()> f) :Get<T&>(f) {}
+	T& ref() { return Get<T&>::val(); };
+	T& val() = delete;
+private:
+	RefGet(const RefGet&) {}
+};
 template<class T>
 class GetSet :public Set<T>, public Get<T> {
 public:
-	void operator=(T v) { Set<T>::operator=(v); }
-	operator T_REF() { return  Get<T>::m_get(); }
-	GetSet(std::function<T_REF()> get) :Get<T>(get){}
-	GetSet(std::function<void(T)> set) :Set<T>(set) {}
-	GetSet(std::function<T_REF()> get, std::function<void(T)> set) :Get<T>(get), Set<T>(set) {}
-	GetSet(std::function<void(T)> set, std::function<T_REF()> get) :Get<T>(get), Set<T>(set) {}
+	void operator=(const T& v) { m_set(v); }
+	void operator=(const T&& v) { m_set(v); }
+	GetSet(std::function<T()> get, std::function<void(const T&)> set) :Get<T>(get), Set<T>(set) {}
+	GetSet(std::function<void(const T&)> set, std::function<T()> get) :Get<T>(get), Set<T>(set) {}
+};
+
+template<class T>
+class RefGetSet :public Set<T>, public Get<T&> {
+public:
+	void operator=(const T& v) { m_set(v); }
+	void operator=(const T&& v) { m_set(v); }
+	RefGetSet(std::function<T&()> get, std::function<void(const T&)> set) :Get<T&>(get), Set<T>(set) {}
+	RefGetSet(std::function<void(const T&)> set, std::function<T&()> get) :Get<T&>(get), Set<T>(set) {}
 };
 
 };
 #undef T_REF
 #define GetVal [this]()
-#define GetRef(type) [this]()->const type&
-#define SetVal(type) [this](type value)
+#define GetRef(type) [this]()->type&
+//#define SetVal(type) [this](type value)
+#define SetRef(type) [this](const type& value)
 #endif //GET_SET_HPP
--- test/main.cpp
+++ test/main.cpp
@@ -11,99 +11,274 @@ struct TestType {
 };
 class FunCallCount{
 public:
+	FunCallCount(FunCallCount& f) {
+		m_count = f.m_count + 1;
+		outStr += m_count;
+		std::cout << "copy  c at " << (long)this % 10000 << " from " << (long)&f % 10000 << std::endl;
+	}
+	//FunCallCount(FunCallCount&& f){
+	//	m_count = f.m_count + 1;
+	//	outStr += m_count;
+	//	std::cout << "copy FunCallCount c at " << (long)this % 10000 << " from " << (long)&f % 10000 << std::endl;
+	//}
 	FunCallCount(){
+		outStr = "FunCallCount";
 		std::cout<<"creat at "<<(long)this%10000<<std::endl;
 	}
-	FunCallCount(int c):m_count(c){
-		std::cout<<"creat c at "<<(long)this%10000<<std::endl;
-	}
-	FunCallCount(const FunCallCount& f){
-		m_count = f.m_count;
-		std::cout<<"copy  c at "<<(long)this%10000 <<   " from " << (long)&f%10000 << std::endl;
-	}
+	//FunCallCount(int c):m_count(c){
+	//	std::cout<<"creat c at "<<(long)this%10000<<std::endl;
+	//}
 	~FunCallCount(){
 		std::cout<<"destroy "<<(long)this%10000<<std::endl;
 	}
-
-private:
+	void operator =(const FunCallCount& f) {
+		m_count = f.m_count + 1;
+		outStr += m_count;
+		std::cout << "copy operator c at " << (long)this % 10000 << " from " << (long)&f % 10000 << std::endl;
+	}
+	operator std::string&() {
+		return outStr;
+	}
 	int m_count;
+private:
+	std::string outStr;
 };
 using namespace lee;
-class Test {
-	int m_id;
-	std::string m_name;
-	TestType m_t;
 
-	int m_testI;
+template<class T>
+class TestSet {
+public:
+	Set<T> mVAL;
+	TestSet() : mVAL(SetRef(T){
+		m_val = value;
+	})
+	{ }
+	T m_val;
+private:
+};
+#define MyTestSet(type,val) { \
+	TestSet<type> t_set; \
+	t_set.mVAL = (val);	\
+	std::cout << t_set.m_val << std::endl;\
+}
+
+
+template<class T>
+class TestGet {
+public:
+	Get<T> mVAL;
+	TestGet() : mVAL(GetVal{
+		std::cout << "this " << (void*)this << " mval: " << &m_val << std::endl;
+		return m_val;
+	})
+	{ }
+	T m_val;
+private:
+};
+#define MyTestGet(type,val) { \
+TestGet<type> t_get; \
+t_get.m_val = (val); \
+type&& t= t_get.mVAL; \
+std::cout << &t << "\t"<< t <<std::endl; \
+}
+
+template<class T>
+class TestGetSet {
+public:
+	GetSet<T> mVAL;
+	TestGetSet() : 
+		mVAL(GetVal{
+			std::cout << "get this " << (void*)this << " &mval: " << &m_val << std::endl;
+			return m_val;
+		}, SetRef(T){ 
+			std::cout << "set this " << (void*)this << " mval: " << m_val << " val: " << value << " &val: " << &value << std::endl;
+			m_val = value; 
+		})
+	{ }
+	T m_val;
+private:
+};
+#define MyTestGetSet(type,val) \
+{ \
+	type v = val;\
+	TestGetSet<type> t_get; \
+	std::cout << "set val" << std::endl;\
+	t_get.mVAL = val; \
+	std::cout << "set v" << &v <<std::endl;\
+	t_get.mVAL = v; \
+	std::cout << "get " <<std::endl;\
+	type&& t= t_get.mVAL; \
+	std::cout << "&mVAL: "<< (void*)&t << " t: "<< t  <<std::endl; \
+}
+
+template<class T>
+class TestRefGet {
+public:
+	RefGet<T> mVAL;
+	TestRefGet() : mVAL( GetRef(T){
+		std::cout << "this " << (void*)this << " mval: " << &m_val ;
+		return m_val;
+	})
+	{ }
+	T m_val;
+};
+#define MyTestRefGet(type,val) { \
+TestRefGet<type> t_get; \
+type& ref = t_get.mVAL;	\
+std::cout << " ref: "<< &ref << std::endl; \
+}
+
+template<class T>
+class TestRefGetSet {
+public:
+	RefGetSet<T> mVAL;
+	TestRefGetSet() :
+		mVAL(GetRef(T){
+			std::cout << "get this " << (void*)this << " &mval: " << &m_val << std::endl;
+			return m_val;
+		}, SetRef(T){
+			std::cout << "set this " << (void*)this << " mval: " << m_val << " val: " << value << " &val: " << &value << std::endl;
+			m_val = value;
+		})
+	{ }
+	T m_val;
+private:
+};
+#define MyTestRefGetSet(type,val) \
+{ \
+	type v = val;\
+	TestRefGetSet<type> t_get; \
+	std::cout << "set val" << std::endl;\
+	t_get.mVAL = val; \
+	std::cout << "set v" << &v <<std::endl;\
+	t_get.mVAL = v; \
+	std::cout << "get " <<std::endl;\
+	type& t= t_get.mVAL; \
+	std::cout << "&mVAL: "<< (void*)&t << " t: "<< t  <<std::endl; \
+}
+
+class Test {
+
+	//int m_id;
+	//std::string m_name;
+	//TestType m_t;
+
+	int m_id;
 	FunCallCount m_testFCallCount;
 public:
-	GetSet<int> ID;
-	GetSet<std::string> Name;
-	GetSet<TestType> TValue;
-	GetSet<int> TEST_I;
-	GetSet<FunCallCount> CALL_COUNT;
-	Test() :
-		ID( GetVal{ 
-				return m_id; 
-			},
-			SetVal(int) {
-				m_id = value; 
-			}),
-		Name(GetVal{
-				return m_name;
-			},
-			SetVal(std::string) {
-				m_name = value;
-			}),
-		TValue(GetVal{
-				return m_t;
-			},
-			SetVal(TestType) {
-				m_t = value;
-			}),
-		TEST_I(GetVal{ return m_testI;},
-			SetVal(int){
-				m_testI = value;
-			}),
-		CALL_COUNT(GetRef(FunCallCount){return m_testFCallCount;},
-			SetVal(FunCallCount){m_testFCallCount = value;})
-		,m_testFCallCount()
+	
+	//GetSet<int> ID;
+	//GetSet<std::string> Name;
+	//GetSet<TestType> TValue;
+	//GetSet<int> TEST_I;
+	
+	RefGet<int> getRefID;
+	Set<FunCallCount> setCALL_COUNT;
+	Get<FunCallCount> getCALL_COUNT;
+	//RefGet<FunCallCount> getRefCALL_COUNT;
+	Test():
+		m_testFCallCount()
+		,setCALL_COUNT(
+			SetRef(FunCallCount) {
+					m_testFCallCount = value;
+			})
 
+		, getCALL_COUNT(
+			GetVal{
+				return m_testFCallCount;
+			}
+		)
+		, getRefID(GetRef(int){return m_id; })
+		//, getRefCALL_COUNT(
+		//	GetRef(FunCallCount) {
+		//		return m_testFCallCount;
+		//	}
+		//)
 	{ }
 
-	void test(){
-		std::cout<<"uninit m_testI\t" << TEST_I << std::endl;
-		TEST_I = 1;
-		//assert(m_testI == 1);
-		std::cout<<"m_testI == " << TEST_I << std::endl;
-		const int& m_i_ref = TEST_I;
-		//*m_i_ref = 100;
-		std::cout<<"m_i_ref == " << TEST_I << std::endl
-		<<"&TEST_I\t" << &m_i_ref << std::endl
-		<<"m_testI\t" << &m_testI << std::endl;
-	}
-	void testCallCount(){
-		{
-			const FunCallCount& f = CALL_COUNT;
+	//void test(){
+	//	Name = "test string";
+	//	std::string name = Name;
+	//	std::cout << name << std::endl;
 
-		}
-	std::cout << std::endl;
-		{
-			CALL_COUNT = FunCallCount(1);
-		}
-		
-	}
+
+	//	std::cout<<"uninit m_testI\t" << TEST_I << std::endl;
+	//	TEST_I = 1;
+	//	//assert(m_testI == 1);
+	//	std::cout<<"m_testI == " << TEST_I << std::endl;
+	//	const int& m_i_ref = TEST_I;
+	//	//*m_i_ref = 100;
+	//	std::cout<<"m_i_ref == " << TEST_I << std::endl
+	//	<<"&TEST_I\t" << &m_i_ref << std::endl
+	//	<<"m_testI\t" << &m_testI << std::endl;
+	//}
+	//void testCallCount(){
+	//	{
+	//		auto& f = (FunCallCount&)CALL_COUNT.$();
+	//		f.m_count = 100;
+	//		
+	//		std::cout << CALL_COUNT.$().m_count  << std::endl;
+	//	}
+	//std::cout << std::endl;
+	//	{
+	//		CALL_COUNT = FunCallCount(1000);
+	//	}
+	//	
+	//}
 };
 
-
 int main(){
-	
-	Test t;
-	t.test();
-	std::cout << std::endl;
-	t.testCallCount();
+	MyTestSet(short, 10);
+	MyTestSet(int, 10);
+	MyTestSet(float, 10.0);
+	MyTestSet(double, 10);
+	MyTestSet(bool, true);
+	MyTestSet(std::string, "hello");
 
-	std::cout << std::endl;
+	MyTestGet(short, 10);
+	MyTestGet(int, 10);
+	MyTestGet(float, 10.0);
+	MyTestGet(double, 10);
+	MyTestGet(bool, true);
+	MyTestGet(std::string, "hello");
+
+	MyTestGetSet(short, 10);
+	MyTestGetSet(int, 10);
+	MyTestGetSet(float, 10.0);
+	MyTestGetSet(double, 10);
+	MyTestGetSet(bool, true);
+	MyTestGetSet(std::string, "hello");
+
+	MyTestRefGet(short, 10);
+	MyTestRefGet(int, 10);
+	MyTestRefGet(float, 10.0);
+	MyTestRefGet(double, 10);
+	MyTestRefGet(bool, true);
+	MyTestRefGet(std::string, "hello");
+
+	MyTestRefGetSet(short, 10);
+	MyTestRefGetSet(int, 10);
+	MyTestRefGetSet(float, 10.0);
+	MyTestRefGetSet(double, 10);
+	MyTestRefGetSet(bool, true);
+	MyTestRefGetSet(std::string, "hello");
+
+	FunCallCount fc;
+	fc.m_count = 1000;
+	std::cout << "create fc" << std::endl;
+	Test t;
+	t.setCALL_COUNT = fc;
+	t.getRefID.ref();
+	//{ TestSet<FunCallCount> t_set; t_set.mVAL = (fc); std::cout << (std::string)t_set.m_val << t_set.m_val.m_count << std::endl; }
+	//MyTestSet(FunCallCount, fc);
+
+
+	//Test t;
+	////t.test();
+	//std::cout << std::endl;
+	////t.testCallCount();
+
+	//std::cout << std::endl;
 
 /*
 	t.ID = 100;
